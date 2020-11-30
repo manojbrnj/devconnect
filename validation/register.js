0b90288f091daab3b2b78be197bb9e966b0de2b9
manoj barange <manojbrnj111@gmail.com>
2020-11-30 14:12:45 +0530

    validation done

--- validation/register.js
+++ validation/register.js
@@ -1,13 +1,50 @@
 const validation = require('validator')
 const isEmpty = require('./is_empty')
 module.exports = function validateRegisterINput(Input){
+    Input.name = !isEmpty(Input.name) ? Input.name:''
+    Input.email = !isEmpty(Input.email) ? Input.email:''
+    Input.password = !isEmpty(Input.password) ? Input.password:''
+    Input.cpassword = !isEmpty(Input.cpassword) ? Input.cpassword:''
 var errors = {
 
 }
-if(!validation.isLength(Input.name,{min:7,max:30})){
+if(!validation.isLength(Input.name,{min:2,max:30})){
 
     errors.name = "Name must be between 2 & 13 character"
 }
+if(validation.isEmpty(Input.name)){
+
+    errors.name = "name field is required"
+}
+if(validation.isEmpty(Input.email)){
+
+    errors.email = "email field is required"
+}
+if(!validation.isEmail(Input.email)){
+
+    errors.email = "Emaili is in valid"
+}
+if(!validation.isLength(Input.password,{min:2,max:30})){
+
+    errors.password = "password must be between 2 & 13 character"
+}
+
+if(validation.isEmpty(Input.password)){
+
+    errors.password = "password field required"
+}
+
+
+if(validation.isEmpty(Input.cpassword)){
+
+    errors.cpassword = "cpassword field required"
+}
+if(!validation.equals(Input.cpassword,Input.password)){
+
+    errors.cpassword = "cpassword must be same as password"
+}
+
+
 return({errors,isValid:isEmpty(errors)})
 
 } 
